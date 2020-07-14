0dbf3f7d38b9ec9f54835f45dc79686905588723
Jacob Ochoa <jacob.ochoa@gmail.com>
2020-07-13 18:25:44 -0700

    fixing rcon password

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ docker run -d \
     -e ICON=https://github.com/borkcraft/bork-scripts/raw/master/icon.png \
     -e 'MOTD=Doko???' \
     -e MODS=https://github.com/borkcraft/bork-mods/raw/master/MixinBootstrap-1.0.3.jar,https://github.com/borkcraft/bork-mods/raw/master/fabric-api-0.14.1%2Bbuild.372-1.16.jar,https://github.com/borkcraft/bork-mods/raw/master/lithium-fabric-mc1.16.1-0.5.1.jar \
-    -e RCON_PASSWORD=$rcon_password
+    -e RCON_PASSWORD=$rcon_password \
     -v /opt/borkcraft:/data \
     -p 25565:25565 \
     --restart unless-stopped \
